5816cfe96841017e6e0bf49b95fddc443df0a21a
sergey.fedorkov <sergey.fedorkov@Win10Krd04.softwarium.net>
2020-11-06 10:36:25 +0300

    selection provider changed

--- market/app/cars/database/providers/CarPositionSelectProvider.java
+++ market/app/cars/database/providers/CarPositionSelectProvider.java
@@ -3,8 +3,16 @@ package cars.database.providers;
 import cars.database.beans.CarPosition;
 
 public class CarPositionSelectProvider {
+	private static final String SELECT_ALL = "SELECT * from market.carposition as entity where entity.id=entity.id";
 
-	public static String getCarPositions(CarPosition carPositions) {
-		return "SELECT * from market.carposition";
+	public static String getCarPositions(CarPosition carPosition) {
+		String result = SELECT_ALL;
+		if (carPosition == null) return result;
+
+		if (carPosition.getOd() != 0) result+=" and od = "+carPosition.getOd();
+		if (carPosition.getPrice() != 0) result+=" and price = "+carPosition.getPrice();
+		if (carPosition.getYearIssue() != null) result+=" and yearIssue = "+carPosition.getYearIssue();
+		
+		return result;
 	}
 }
