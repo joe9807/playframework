9a8a71a9a6dbba2ba3f78c17faf1c50cec0604bf
sergey.fedorkov <sergey.fedorkov@Win10Krd04.softwarium.net>
2020-11-06 11:11:29 +0300

    rename

--- market/test/controllers/MarketControllerTest.java
+++ market/test/controllers/MarketControllerTest.java
@@ -12,7 +12,7 @@ import static play.mvc.Http.Status.OK;
 import static play.test.Helpers.GET;
 import static play.test.Helpers.route;
 
-public class HomeControllerTest extends WithApplication {
+public class MarketControllerTest extends WithApplication {
 
     @Override
     protected Application provideApplication() {
@@ -21,12 +21,9 @@ public class HomeControllerTest extends WithApplication {
 
     @Test
     public void testIndex() {
-        Http.RequestBuilder request = new Http.RequestBuilder()
-                .method(GET)
-                .uri("/");
+        Http.RequestBuilder request = new Http.RequestBuilder().method(GET).uri("/");
 
         Result result = route(app, request);
         assertEquals(OK, result.status());
     }
-
 }
