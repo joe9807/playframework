4d2a7691192aaa5a59b68d916db3d8e2595561df
sergey.fedorkov <sergey.fedorkov@Win10Krd04.softwarium.net>
2020-11-06 12:33:49 +0300

    test

--- market/test/controllers/MarketControllerTest.java
+++ market/test/controllers/MarketControllerTest.java
@@ -1,15 +1,20 @@
 package controllers;
 
 import static org.junit.Assert.assertEquals;
-import static play.mvc.Http.Status.OK;
 import static play.mvc.Http.Status.NOT_FOUND;
+import static play.mvc.Http.Status.OK;
 import static play.test.Helpers.GET;
+import static play.test.Helpers.contentAsString;
 import static play.test.Helpers.route;
 
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
+import cars.database.beans.CarKind;
 import play.Application;
 import play.inject.guice.GuiceApplicationBuilder;
+import play.libs.Json;
 import play.mvc.Http;
 import play.mvc.Http.RequestBuilder;
 import play.mvc.Result;
@@ -37,5 +42,25 @@ public class MarketControllerTest extends WithApplication {
 
       Result result = route(app, request);
       assertEquals(NOT_FOUND, result.status());
+    }
+    
+    public void testList() {
+    	CarKind carKind = new CarKind("1", "2");
+    	//Form form = mock(Form.class);
+    	//Http.RequestBuilder request = new Http.RequestBuilder().method(POST).bodyText("name=CarKindName&country=CarKindCountry").uri("/carKind/add");
+    	Http.RequestBuilder request = Helpers.fakeRequest("POST", "/carKind/add").header("Content-Type", "application/x-www-form-urlencoded").bodyText("name=CarKindName&country=CarKindCountry");
+    	
+    	Result result = route(app, request);
+
+        assertEquals(200, result.status());
+
+        //JsonNode listOfPosts = contentAsJson(result);
+        //Optional<PostResource> post = findPostByTitle(listOfPosts, "title-of-post-123");
+        //assertTrue(post.isPresent());
+    }
+    
+    private JsonNode contentAsJson(Result result) {
+        final String responseBody = contentAsString(result);
+        return Json.parse(responseBody);
     }
 }
