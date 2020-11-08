6cc2ef4e620890a8c98a1119c07fea01e1c577b0
sergey.fedorkov <sergey.fedorkov@Win10Krd04.softwarium.net>
2020-11-08 20:06:24 +0300

    selection provider refactoring

--- market/app/cars/database/providers/CarPositionSelectProvider.java
+++ market/app/cars/database/providers/CarPositionSelectProvider.java
@@ -3,13 +3,9 @@ package cars.database.providers;
 import cars.database.beans.CarPosition;
 
 public class CarPositionSelectProvider {
-	private static final String SELECT_ALL = "SELECT * from market.carposition";
-
 	public static String getCarPositions(CarPosition carPosition) {
-		String result = SELECT_ALL;
-		if (carPosition == null) return result;
+		String result = "SELECT * from market.carposition where";
 
-		result+= " where";
 		if (carPosition.getKind() != null && carPosition.getKind().getName() != null) result+=" kindId = (SELECT id FROM market.carkind WHERE name=#{kind.name}) and";
 		if (carPosition.getModel() != null && carPosition.getModel().getName() != null) result+=" modelId = (SELECT id FROM market.carmodel WHERE name=#{model.name}) and";
 		if (carPosition.getOd() != 0) result+=" od = #{od} and";
